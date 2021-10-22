486eeb6927510ddc7cefd86395c2ff1be042ffd1
SNValadez <snvaladez13@gmail.com>
2021-10-22 16:51:14 -0700

    get /notes and return

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,10 @@
 const router = require('express').Router();
 const db = require("../db/db.json")
+
+router.get("/notes", (req, res) => {
+    
+    return res.json(db);
+});
+
+console.log(db);
+module.exports = router;
