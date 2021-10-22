50532175349b863a634846129ea0297f61f49e38
SNValadez <snvaladez13@gmail.com>
2021-10-22 16:29:38 -0700

    app.use and app.listen for PORT

--- server.js
+++ server.js
@@ -9,3 +9,11 @@ app.use(express.static("./public"));
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
