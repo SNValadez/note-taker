df3221e8d7401b63e8d89a9f16acf3d70fde4cc0
SNValadez <snvaladez13@gmail.com>
2021-10-22 15:52:40 -0700

    static ./public and urlencoded

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ const PORT = process.env.PORT || 3002;
 const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.static("./public"));
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
