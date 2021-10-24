f79db3a01de2f3a8ae31c4392583a6ef3ac86489
SNValadez <snvaladez13@gmail.com>
2021-10-24 11:47:44 -0700

    app.get /

--- server.js
+++ server.js
@@ -14,6 +14,17 @@ app.use(express.json());
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
+app.get("api/notes", (req, res) => {
+    console.log(db);
+    res.json(db);
+})
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
